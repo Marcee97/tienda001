176bc595400c9f2913ee4d6527d418455dc06f08
marcee <marceloquadrilatero@gmail.com>
2026-06-17 22:33:46 -0300

    add diseño a welcome carrito menu desp

--- src/ui/Welcome.jsx
+++ src/ui/Welcome.jsx
@@ -9,38 +9,46 @@ export const Welcome = () => {
 
   return (
     <section className="welcome">
-       <div
-          className="welcome__boton--toggle"
-          onClick={() => setColorNegro((prev) => !prev)}
-        >
+      <div
+        className="welcome__boton--toggle"
+        onClick={() => setColorNegro((prev) => !prev)}
+      >
         <div className="welcome__cont-boton-punto">
-          <div className="welcome__boton-punto"/>
+          <div className="welcome__boton-punto" />
         </div>
         <p className="welcome__cont-boton-text"></p>
-        </div>
+      </div>
       <div className="welcome__bg">
-        
         <div
           className="welcome__img welcome__img--blanca"
-          style={{ backgroundImage: "url('https://res.cloudinary.com/dren5qsyw/image/upload/v1781545296/ChatGPT_Image_15_jun_2026_02_41_06_p.m_n9dgm7.png')" }}
+          style={{
+            backgroundImage:
+              "url('https://res.cloudinary.com/dren5qsyw/image/upload/v1781545296/ChatGPT_Image_15_jun_2026_02_41_06_p.m_n9dgm7.png')",
+          }}
         />
         <div
           className={`welcome__img welcome__img--negra ${colorNegro ? "visible" : ""}`}
-          style={{ backgroundImage: "url('https://res.cloudinary.com/dren5qsyw/image/upload/v1781543863/ChatGPT_Image_15_jun_2026_02_13_58_p.m_qhkueo.png')" }}
+          style={{
+            backgroundImage:
+              "url('https://res.cloudinary.com/dren5qsyw/image/upload/v1781543863/ChatGPT_Image_15_jun_2026_02_13_58_p.m_qhkueo.png')",
+          }}
         />
       </div>
 
       <div className="welcome__contenido">
-        <h6 className="welcome__subtitulo">
-          {colorNegro ? "Remeras negras" : "Remeras blancas"}
-        </h6>
-       
-       {/* <button
-          className="welcome__boton"
+        <p className="welcome__contenido--titulo">Remeras</p>
+        <div className="welcome__contenido--subtitulo">
+
+        <h3>BLANCAS.</h3>
+        <h3>NEGRAS.</h3>
+        </div>
+        <button
+          className="welcome__boton-open-shop"
           onClick={() => setOpenCloseModalCompra(true)}
         >
-          Start
-        </button>*/}
+          VER COLECCION
+        </button>
+       
       </div>
     </section>
   );
