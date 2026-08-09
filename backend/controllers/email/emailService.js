d1879efb2fe525dd5bbb2a59b31d456a95d0da7e
marcee <marceloquadrilatero@gmail.com>
2026-08-09 14:29:04 -0300

    fix formato email

--- backend/controllers/email/emailService.js
+++ backend/controllers/email/emailService.js
@@ -2,31 +2,34 @@ const { Resend } = require("resend");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const enviarEmailCompra = async ({ email, nombre, carrito, total }) => {
-  console.log(
-    email,
-    nombre,
-    carrito,
-    total,
-    "esta es la info que llega al emailService",
-  );
+const enviarEmailCompra = async ({ envio, carrito, total }) => {
+  console.log(envio, "esto en el formulario que llega emnail service")
+  const { nombre, email, calle, numero, ciudad, provincia, codigoPostal } = envio;
+
+  console.log(envio, carrito, total, "esta es la info que llega al emailService");
+
   await resend.emails.send({
-    from: "Valley <onboarding@resend.dev>",
+    from: "Hisbell <onboarding@resend.dev>",
     to: email,
-    subject: "✅ Confirmación de compra - Valley",
+    subject: "✅ Confirmación de compra - Hisbell",
     html: `
-  <h1>Gracias ${nombre}!</h1>
-  <p>Tu compra fue confirmada.</p>
-  ${carrito.map((item) => `
-    <div>
-      <p><strong>${item.nombre}</strong></p>
-      <p>Talle: ${item.talle}</p>
-      <p>Cantidad: ${item.cantidad}</p>
-      <p>Precio: $${item.precio}</p>
-    </div>
-  `).join('')}
-  <p><strong>Total: $${total}</strong></p>
-`,
+      <h1>Gracias ${nombre}!</h1>
+      <p>Tu compra fue confirmada.</p>
+      <p>Dirección: ${calle} ${numero}, ${ciudad}, ${provincia} (CP ${codigoPostal})</p>
+      ${carrito
+        .map(
+          (item) => `
+        <div>
+          <p><strong>${item.nombre}</strong></p>
+          <p>Talle: ${item.talle}</p>
+          <p>Cantidad: ${item.cantidad}</p>
+          <p>Precio: $${item.precio}</p>
+        </div>
+      `,
+        )
+        .join("")}
+      <p><strong>Total: $${total}</strong></p>
+    `,
   });
 };
 
